b0dddd9c583ab5ad4815ac8c45cb495016be83fc
allania7med11 <allania7med11@gmail.com>
2021-01-22 13:35:26 +0100

    fix

--- myNewApp/src/app/app-routing.module.ts
+++ myNewApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TrackingStudentComponent } from './components/tracking-student/tracking-student.component';
+import { TableBasicExampleComponent } from './table-basic-example/table-basic-example.component';
 
 const routes: Routes = [
-  {path:'tracking', component :TrackingStudentComponent}
+  {path:'tracking', component :TrackingStudentComponent},
+  {path:'table', component :TableBasicExampleComponent},
 ];
 
 @NgModule({
